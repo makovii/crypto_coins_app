0c7206e98f393485713691ca99994602c300aedb
makovii <viktorkaktysikov@gmail.com>
2024-03-07 01:08:39 +0200

    added dependency injection, abstract class, use get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:crypto_coins_list/theme/crypto_coins_list_app.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'repositories/crypto_coins/crypto_coins.dart';
 
 void main() {
+  GetIt.I.registerSingleton<AbstractCoinsRepository>(CryptoCoinsRepository(dio: Dio()));
   runApp(const CryptoCoinsListApp());
 }
 
---
+++ lib/repositories/crypto_coins/abstract_coins_repository.dart
@@ -0,0 +1,5 @@
+import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
+
+abstract class AbstractCoinsRepository {
+  Future<List<CryptoCoin>> getCoinsList();
+}
