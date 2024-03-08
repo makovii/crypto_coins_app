791993ac9d47fe7ba49e749efaad2bd8810eae1f
makovii <viktorkaktysikov@gmail.com>
2024-03-08 13:48:33 +0200

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:crypto_coins_list/firebase_options.dart';
 import 'package:crypto_coins_list/theme/crypto_coins_list_app.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'repositories/crypto_coins/crypto_coins.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final app = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // talker initialization
+  // talker.info(app.options.projectId);
+
   GetIt.I.registerSingleton<AbstractCoinsRepository>(CryptoCoinsRepository(dio: Dio()));
   runApp(const CryptoCoinsListApp());
 }
